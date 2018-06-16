95d84fd9724f7a7dc376e907e7123169b872d8d9
VeganShe <veganshe@163.com>
2018-06-16 23:40:29 +0800

    Added user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    /**
+     * 用户详细页
+     * @param Request $request
+     */
+    public function index(Request $request, $id) {
+    	
+    	$userinfo = DB::table('user_profile')->where('uid',$id)->first();
+    	$user_extras = DB::table('user_extras')->where('user_id',$id)->first();
+
+    	$user = [
+    		'uid' => $userinfo->uid,
+    		'name' => $userinfo->username,
+    		'avatar' => $userinfo->avatar,
+    		'present' => $userinfo->present,
+    		'followers_count' => $user_extras->followers_count,
+    		'followings_count' => $user_extras->followings_count,
+    		'tags_count' => $user_extras->tags_count,
+    		'posts_count' => $user_extras->posts_count
+    	];
+        return response()->json($user);
+    }
+
+    public function profile($id) {
+
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Contracts\Routing\Registrar as RouteContract;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,42 @@ use Illuminate\Http\Request;
 |
 */
 
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+
+// 测试接口访问
+//Route::get('hello',function(Request $request) {
+//	echo $request->ip();
+//});
+
+Route::group(['prefix' => 'v2'], function(RouteContract $api) {
+
+    // Demo
+    $api->get('ip', function (Request $request) {
+        echo $request->ip();
+    });
+
+    // 用户组
+    $api->group(['prefix' => 'user'], function(RouteContract $api) {
+        // 用户页面
+        $api->get('/{id}', 'UserController@profile');
+
+        $api->get('/{id}/abc',function ($id) {
+            echo '我的id:'.$id;
+        });
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
